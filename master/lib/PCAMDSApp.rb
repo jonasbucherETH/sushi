b1404a94263d7958d50c74a5e8f7d6bc2014624b
Jonas Bucher <jobucher@fgcz-net.unizh.ch>
2022-12-14 17:01:47 +0100

    added pca ruby app

---
+++ master/lib/PCAMDSApp.rb
@@ -0,0 +1,39 @@
+
+# encoding: utf-8
+
+require 'sushi_fabric'
+require_relative 'global_variables'
+include GlobalVariables
+
+class PCAMDSApp <  SushiFabric::SushiApp
+  def initialize
+    super
+    @name = 'PCAMDS'
+    @params['process_mode'] = 'DATASET'
+    @analysis_category = 'Variants'
+    @description =<<-EOS
+vcf-stats<br/>
+    EOS
+    @required_columns = ['Name', 'Filtered VCF', 'Dummy']
+    @required_params = ['name']
+    @params['cores'] = '1'
+    @params['ram'] = '50'
+    @params['scratch'] = '100'
+    @params['name'] = 'pca_mds'
+    @params['mail'] = ""
+    @modules = ["Tools/PLINK/1.9beta6.21"]
+    @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
+  end
+  def next_dataset
+    report_file = File.join(@result_dir, @params['name'])
+    report_link = File.join(report_file, '00index.html')
+    {'Name'=>@params['name'],
+     'Report [File]'=>report_file,
+     'Html [Link]'=>report_link,
+    }.merge(extract_columns(@inherit_tags))
+  end
+  def commands
+    run_RApp("EzAppVcfStats", lib_path:  "/srv/GT/analysis/jonas/R_LIBS")
+    #command = "vcf-stats #{File.join("$GSTORE_DIR", @dataset[0]['Filtered VCF [File]'])} -p #{@params['name']}/vcf_stats"
+  end
+end
