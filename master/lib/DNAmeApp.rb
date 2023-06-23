9a3e9099022aa60ac772272863101c5259a5f8fb
Jonas Bucher <jobucher@student.ethz.ch>
2023-06-23 13:53:53 +0200

    add tools, params from all parts; incl grouping param

--- master/lib/DNAmeApp.rb
+++ master/lib/DNAmeApp.rb
@@ -14,14 +14,27 @@ class DNAmeApp <  SushiFabric::SushiApp
     @description =<<-EOS
 DNA methylation analysis<br/>
     EOS
-    @required_columns = ['Name','Read1']
+    @required_columns = ['Name','Read1', 'COV', 'BAM']
     @required_params = ['name']
     @params['cores'] = '1'
     @params['ram'] = '50'
     @params['scratch'] = '100'
+    @params['refBuild'] = ref_selector
+    @params['refBuild', 'description'] = 'Select one ending with /Annotation/Version_[...] or /Annotation/Release_[...]'
+    #@params['refFeatureFile'] = 'genes.gtf'
+    #@params['refFeatureFile'] = '../../Sequence/WholeGenomeFasta/genome.fa'
+    @params['grouping'] = ''
+    @params['grouping', 'description'] = 'Specify the column name of your co-variate to 
+    split the samples into groups for testing for differential methylation. Make sure the
+    column name is in the format "NAME [Factor]" or "NAME [Numeric]"'
+    @params['sampleGroup'] = '' ## Note: this will be a selector defined by Factor tagged column
+    @params['sampleGroup', 'description'] = 'Test group. sampleGroup should be different from refGroup'
+    @params['refGroup'] = '' ## Note: this will be a selector defined by Factor tagged column
+    @params['refGroup', 'description'] = 'Control group. refGroup should be different from sampleGroup'
+    @params['biomart_selection'] = biomart_selector
     @params['name'] = 'DNAme'
     @params['mail'] = ""
-    @modules = ["Tools/HOMER/4.11"]
+    @modules = ["Dev/R", "Tools/HOMER", "Tools/BEDTools"]
     @inherit_tags = ["Factor", "B-Fabric", "Characteristic"]
   end
   def next_dataset
